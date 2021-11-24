044026d780a3b10e6a25a40d2b1f4b44e4b64560
bettyvng <betty.vg8@gmail.com>
2021-11-24 19:42:31 +0000

    implement function to determine midpoint of array

--- middle.js
+++ middle.js
@@ -18,3 +18,29 @@ const assertArraysEqual = function(array1, array2) {
   return(console.log(`🛑🛑🛑  Assertion Failed: ${array1} !== ${array2}`));
   return (console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`));
 }
+
+const middle = function (array) {
+  let returnArray = [];
+  let midpoint = Math.floor(array.length/2);
+
+  if (array.length <= 2) {
+    return returnArray;
+
+  } else if (array.length % 2 === 0)  {
+    returnArray.unshift(array[midpoint]);
+    returnArray.unshift(array[midpoint - 1]);
+    return returnArray;
+
+  } else {
+    returnArray.unshift(array[midpoint]);
+    return returnArray;
+  }
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([2, 2]), []); 
