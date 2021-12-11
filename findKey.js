d56761a48b8902fd40df62c66598fec2ab2ef39f
Betty Vuong <bettyvng@BZ-macbook.local>
2021-12-11 17:10:14 -0500

    findKey implementation

--- findKey.js
+++ findKey.js
@@ -8,15 +8,29 @@ const assertEqual = function(actual, expected) {
 };
 
 // findKey Function
-const findKey = function(object, key)
+const findKey = function(object, callback) {
+  let resultKey;
+  Object.keys(object).some((key) => {
+    if (callback(object[key])) {
+      resultKey = key;
+      return true;
+    }
+    return false;
+  });
+  return resultKey;
+}
 
 // test case
 
-findKey({
+const testObject = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+};
+assertEqual(findKey(testObject, x => x.stars === 2), 'noma');
+assertEqual(findKey(testObject, x => x.stars === 3), 'Akaleri');
+assertEqual(findKey(testObject, x => x.stars === 1), 'Blue Hill');
+assertEqual(findKey(testObject, x => x.stars === 5), undefined);
